57ed07b91e0a8484c7f5d36ca94e1b4870b6e125
riadhh-302005 <30ria10@gmail.com>
2026-05-17 23:14:43 +0530

    fixed frontend route

--- server.js
+++ server.js
@@ -13,9 +13,7 @@ app.use(express.static('public'));
 
 app.use('/api/employees', employeeRoutes);
 
-app.get('/', (req, res) => {
-    res.send("Employee Management Server Running");
-});
+
 
 const PORT = process.env.PORT || 5000;
 
